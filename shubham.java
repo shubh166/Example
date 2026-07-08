ad5583e0263845b8f0f521e0f317222ec7afeb93
shubh166 <shubh14898@gmail.com>
2026-07-08 16:17:53 +0530

    Update shubham.java

--- shubham.java
+++ shubham.java
@@ -1,26 +1,14 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 import javax.servlet.http.HttpServletRequest;
 
 public class VulnerableTest {
-
     public void test(HttpServletRequest request) throws Exception {
-
-        // User-controlled input
         String userInput = request.getParameter("id");
 
-        Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost/test", "username", "password");
-
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test");
         Statement stmt = conn.createStatement();
 
-        // Vulnerable: SQL query is built using string concatenation
         String query = "SELECT * FROM users WHERE id = " + userInput;
-
         stmt.executeQuery(query);
-
-        stmt.close();
-        conn.close();
     }
 }
