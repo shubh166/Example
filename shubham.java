9e02ef63c787116532bd84998826e4a7a6aa06f2
Shubham Kumar Singh <shubham.kumar.singh@pwc.com>
2026-07-14 07:43:53 +0530

    going

--- shubham.java
+++ shubham.java
@@ -1,13 +1,16 @@
+import java.util.*;
 import java.sql.*;
 import javax.servlet.http.HttpServletRequest;
-
+public static void main(String args[])
+{
 public class VulnerableTest {
     public void test(HttpServletRequest request) throws Exception {
         String userInput = request.getParameter("id");
 
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test");
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
+        Statement stmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
         stmt.setString(1, userInput);
         stmt.executeQuery();
     }
+}
 }
