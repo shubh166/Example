864a733df610acd47d84cac1859c8ef21bb5ff6d
Shubham Kumar Singh <shubham.kumar.singh@pwc.com>
2026-07-14 07:51:08 +0530

    itgoood

--- VulnerableTest.java
+++ VulnerableTest.java
@@ -1,16 +1,14 @@
-import java.util.*;
 import java.sql.*;
 import javax.servlet.http.HttpServletRequest;
-public static void main(String args[])
-{
+
 public class VulnerableTest {
     public void test(HttpServletRequest request) throws Exception {
         String userInput = request.getParameter("id");
 
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test");
-        Statement stmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?");
-        stmt.setString(1, userInput);
-        stmt.executeQuery();
+        Statement stmt = conn.createStatement();
+
+        String query = "SELECT * FROM users WHERE id = " + userInput;
+        stmt.executeQuery(query);
     }
-}
 }
