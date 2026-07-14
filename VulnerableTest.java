b327441d22993ccb78026159363a52e03fa07493
Shubham Kumar Singh <shubham.kumar.singh@pwc.com>
2026-07-14 17:52:26 +0530

    pushed

---
+++ VulnerableTest.java
@@ -0,0 +1,16 @@
+//Code for SAST vulnerabilitieswdwwfwf
+
+import java.sql.*;
+import javax.servlet.http.HttpServletRequest;
+
+public class VulnerableTest {
+    public void test(HttpServletRequest request) throws Exception {
+        String userInput = request.getParameter("id");
+
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test");
+        Statement stmt = conn.createStatement();
+
+        String query = "SELECT * FROM users WHERE id = " + userInput;
+        stmt.executeQuery(query);
+    }
+}
